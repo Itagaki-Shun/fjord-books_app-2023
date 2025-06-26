79003476bcc56c3227ec0ccb6eaec3526531509a
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-26 16:57:35 +0900

    configure_permitted_parametersメソッドを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zip, :address, :introduction])
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zip address introduction])
+  end
 end
