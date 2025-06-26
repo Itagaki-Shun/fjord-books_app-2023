b0af365114280f8d3e8bce0ee46019aebf262b7f
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-26 15:43:10 +0900

    ユーザーの一覧ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get 'users/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   resources :books
+  resources :users, only: [:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
