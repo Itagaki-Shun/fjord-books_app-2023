d827a28961f1a40c0a17ad738fb76173ae281b88
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-27 11:45:42 +0900

    letter_opener_webを導入、パスワードリセットメール確認機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'mypage', to: 'users#mypage'
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener'
   resources :books
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
