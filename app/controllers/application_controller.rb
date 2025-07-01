3dacfc9a5a36f2e360e51e7adf21a1c294fbf6f2
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-01 09:12:19 +0900

    メソッドの引数とパラメータを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
-    books_path(resource)
+  def after_sign_in_path_for(_resource)
+    books_path
   end
 
   def after_sign_out_path_for(_resource_or_scope)
