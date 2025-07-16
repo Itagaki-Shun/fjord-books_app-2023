eeb422311120c4b1bafc552943f91bf82cca974b
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-17 08:53:55 +0900

    日報の編集機能を追加 機能追加
    [目的]
    - 特定の日報を編集できるようにする
    
    [変更内容]
    - `ReportsController`に`edit`と`update`アクションを追加
    - `edit`アクションで編集フォームを表示するよう実装
    - `update`アクションでフォームから送信されたデータで日報を更新する処理を実装
    - 更新成功時は詳細ページへリダイレクトし、失敗時は編集ページを再表示
    - `before_action`に`:edit`, `:update`を追加し、対象の日報を事前に取得するようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_book, only: %i[show]
+  before_action :set_book, only: %i[show edit update]
 
   def index
     @reports = Report.order(:id).page(params[:page])
@@ -13,6 +13,8 @@ class ReportsController < ApplicationController
 
   def show; end
 
+  def edit; end
+
   def create
     @report = Report.new(report_params)
 
@@ -22,6 +24,18 @@ class ReportsController < ApplicationController
         format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+        format.json { render :show, status: :ok, location: @report }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
