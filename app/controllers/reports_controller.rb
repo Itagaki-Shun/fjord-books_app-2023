d5de1ab093daac77febc6fb6a004930a7305d4a7
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-16 16:42:20 +0900

    新規日報作成機能の実装 機能追加
    [目的]
    - フォームから送信された新しい日報を作成し、データベースに保存するロジックを実装。
    
    [変更内容]
    - `ReportsController`に`create`アクションを追加
    - フォームから送信されたパラメータを使用して日報を新規作成し、データベースに保存する処理を実装
    - 保存成功時には詳細ページへリダイレクトし、失敗時には新規作成フォームを再表示するよう実装
    - Strong Parametersを使用して、安全にパラメータを受け取るための`report_params`メソッドを定義

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,4 +8,24 @@ class ReportsController < ApplicationController
   def new
     @report = Report.new
   end
+
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content)
+  end
 end
