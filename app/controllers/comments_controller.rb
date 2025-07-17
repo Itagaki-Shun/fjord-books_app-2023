ff8203eace8097cc86aee229b77a9047cedefeae
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-17 13:55:13 +0900

    コメントを投稿できるように`create`アクションを追加 機能追加
    [目的]
    - 本や日報にコメントを投稿できるようにする
    
    [変更内容]
    - `create`アクションを定義
    - before_actionでコメント対象のモデル（Book or Report）を判定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,34 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_commentable
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.commentable = @commentable
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_commentable
+    if params[:book_id]
+      @commentable = Book.find(params[:book_id])
+    elsif params[:report_id]
+      @commentable = Report.find(params[:report_id])
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
