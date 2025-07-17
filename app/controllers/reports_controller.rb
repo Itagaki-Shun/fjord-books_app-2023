dd42de92427212a122948bb146b4a098702af689
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-17 09:34:26 +0900

    メソッド名を修正 修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_book, only: %i[show edit update destroy]
+  before_action :set_report, only: %i[show edit update destroy]
 
   def index
     @reports = Report.order(:id).page(params[:page])
@@ -52,7 +52,7 @@ class ReportsController < ApplicationController
 
   private
 
-  def set_book
+  def set_report
     @report = Report.find(params[:id])
   end
 
