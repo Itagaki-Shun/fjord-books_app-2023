2c6519aa83c78f5ba85b20fa5fdd85e0f5b4aa9e
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-30 11:11:23 +0900

    `User`モデルの`name_or_email`メソッドのテストを追加 テスト追加
    [目的]
    - ユーザーの表示名機能の動作を確認する
    
    [変更内容]
    - `name_or_email`メソッドのテスト(`表示名`)を追加
    - 名前が空の場合はemailが返されることを確認
    - 名前がある場合は名前が返されることを確認

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '表示名' do
+    user = User.new(email: 'bob@example.com', name: '')
+    assert_equal 'bob@example.com', user.name_or_email
+
+    user.name = 'ボブ'
+    assert_equal 'ボブ', user.name_or_email
+  end
 end
