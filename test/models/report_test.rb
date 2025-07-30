0346a30a67a3227ab8d8150e99dd8625e250c3fc
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-30 14:15:25 +0900

    `Report`モデルの`create_on`メソッドのテストを追加 テスト追加
    [目的]
    - 日報の作成日取得機能の動作を確認する
    
    [変更内容]
    - `created_on`メソッドのテスト(`created_onで作成日を取得できる`)を追加
    - `created_at`から日付部分のみ取得できることを確認
    - 特定日時（1995年12月21日）での動作確認を追加
    - 戻り値がDateクラスであることを確認

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,4 +17,24 @@ class ReportTest < ActiveSupport::TestCase
     assert bob_report.editable?(bob)
     refute bob_report.editable?(alice)
   end
+
+  test 'created_onで作成日を取得できる' do
+    alice = users(:alice)
+    alice_report = reports(:alice_report)
+
+    expected_date = alice_report.created_at.to_date
+    assert_equal expected_date, alice_report.created_on
+
+    # 特定の日時の場合
+    travel_to Time.zone.parse('1995-12-21 12:00:00') do
+      report = Report.create!(
+        user: alice,
+        title: 'Rubyに初めて触った日',
+        content: 'これからが楽しみ'
+      )
+
+      assert_equal Date.new(1995, 12, 21), report.created_on
+      assert_instance_of Date, report.created_on
+    end
+  end
 end
