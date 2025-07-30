253bc845a9a45a2d6cea41ee85d13444015d3b43
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-30 13:23:08 +0900

    `Report`モデルの`editable?`メソッドのテストを追加 テスト追加
    [目的]
    - 日報の編集権限制御機能の動作を確認する
    
    [変更内容]
    - `editable?`メソッドのテスト(`レポート編集は作成者のみ可能`)を追加
    - 自分の日報は編集可能であることを確認
    - 他人の日報は編集不可であることを確認

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'レポート編集は作成者のみ可能' do
+    alice = users(:alice)
+    bob = users(:bob)
+
+    # aliceの日報の編集機能について
+    alice_report = reports(:alice_report)
+    assert alice_report.editable?(alice)
+    refute alice_report.editable?(bob)
+
+    # bobの日報の編集機能について
+    bob_report = reports(:bob_report)
+    assert bob_report.editable?(bob)
+    refute bob_report.editable?(alice)
+  end
 end
