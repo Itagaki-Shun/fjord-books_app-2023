1fd8e56524f7422d19649eeb70c99c82bb5bd25a
itagaki <itagaki_shun@vitallead.co.jp>
2025-08-01 16:47:39 +0900

    `Mention`モデルのアソシエーション追加 機能追加
    [目的]
    - 日報間のメンション関係を定義する
    
    [変更内容]
    - `Mention`モデルに`belongs_to`アソシエーションを追加
    - `Report`モデルに`has_many`アソシエーションを追加
    - `active_mentions`と`passive_mentions`でメンション関係を定義

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_mentions, class_name: 'Mention', foreign_key: 'mentioning_id', dependent: :destroy, inverse_of: :mentioning
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mentioned_id', dependent: :destroy, inverse_of: :mentioned
+
   validates :title, presence: true
   validates :content, presence: true
 
