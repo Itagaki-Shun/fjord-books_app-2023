7b413ee3f3c79c86cbe950263e8c0db5feb828b5
itagaki <itagaki_shun@vitallead.co.jp>
2025-08-01 10:16:59 +0900

    ルーティングをコントローラに合わせて修正 修正
    [目的]
    - 専用のコメントコントローラーにルーティングするため
    
    [変更内容]
    - `books`の`comments`を`book_comments`コントローラーにルーティング
    - `reports`の`comments`を`report_comments`コントローラーにルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: %i(create destroy)
+    resources :comments, controller: 'book_comments', only: %i(create destroy)
   end
 
   resources :users, only: %i(index show)
 
   resources :reports do
-    resources :comments, only: %i(create destroy)
+    resources :comments, controller: 'report_comments', only: %i(create destroy)
   end
 end
