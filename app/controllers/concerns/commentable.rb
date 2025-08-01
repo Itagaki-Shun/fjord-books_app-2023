e7fbee8004b631620f1d85829a2beccd2cf55a07
itagaki <itagaki_shun@vitallead.co.jp>
2025-08-01 10:10:19 +0900

    `Commetable concern`モジュールに、CRUD機能を追加 機能追加
    [目的]
    - 本へのコメントと日報のコメントのコントローラを分けた時の共通の処理をモジュール化する
    
    [変更内容]
    - `create`アクションを追加
    - `destroy`アクションを追加
    - `comment_params`メソッドを追加

--- app/controllers/concerns/commentable.rb
+++ app/controllers/concerns/commentable.rb
@@ -2,4 +2,30 @@
 
 module Commentable
   extend ActiveSupport::Concern
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.commentable = @commentable
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      flash[:alert] = @comment.errors.full_messages.join
+      render "#{@commentable.model_name.route_key}/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @commentable = @comment.commentable
+
+    @comment.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
